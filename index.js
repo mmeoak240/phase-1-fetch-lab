58674e3a6f3e132766ebaa2063c65be688f4aaa3
Michael Meoak <mmeoak240@gmail.com>
2022-03-16 11:23:25 -0700

    commit tree e2f9f3b11eb30060f91e4806adf7ff43a0e55e96

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -9,6 +10,7 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    console.log(books)
   });
 }
 
